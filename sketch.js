73aada198befee92b7e998dc9afb217c04be9bf4
scopeGL <seroleal@gmail.com>
2026-01-23 11:19:17 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,9 +11,15 @@ function setup() {
 
 function draw() {
   shader(kaleidoShader);
+
+  kaleidoShader.setUniform('u_resolution', [width, height]);
+  kaleidoShader.setUniform('u_time', millis() / 1000.0);
+
   rect(-width / 2, -height / 2, width, height);
 }
+
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
+
