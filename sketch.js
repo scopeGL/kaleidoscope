8e089998cc58e8b6def1cca4357b61df87e1da48
scopeGL <seroleal@gmail.com>
2026-01-23 10:50:25 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,23 +1,8 @@
-let kaleidoShader;
-
-function preload() {
-  kaleidoShader = loadShader('kaleido.vert', 'kaleido.frag');
-}
-
-function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
-  noStroke();
-}
-
 function draw() {
   shader(kaleidoShader);
 
   kaleidoShader.setUniform('u_resolution', [width, height]);
   kaleidoShader.setUniform('u_time', millis() / 1000.0);
 
-  rect(-width / 2, -height / 2, width, height);
-}
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+  rect(0, 0, width, height);
 }
