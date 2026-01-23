c7585e53a023a1f03b7c0eb8b6b08b5280479661
scopeGL <seroleal@gmail.com>
2026-01-23 10:18:08 -0300

    Update kaleido.js

--- kaleido.js
+++ kaleido.js
@@ -1,13 +1,7 @@
-let img;
-
-function preload() {
-  img = loadImage('imagen.jpg'); // imagen chica, ya comprobada
-}
-
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   angleMode(DEGREES);
-  textureMode(NORMAL);
+  colorMode(HSB, 360, 100, 100, 100);
   noStroke();
 }
 
@@ -16,27 +10,36 @@ function draw() {
 
   let slices = 12;
   let angle = 360 / slices;
-  let radius = min(width, height) * 0.6;
+  let radius = min(width, height) * 0.45;
 
-  rotateZ(frameCount * 0.1);
+  rotateZ(frameCount * 0.15);
 
   for (let i = 0; i < slices; i++) {
     push();
     rotateZ(i * angle);
 
+    // espejo alternado
     if (i % 2 === 1) {
       scale(1, -1, 1);
     }
 
-    beginShape();
-    texture(img);
-
-    vertex(0, 0, 0, 0.5, 0.5);
-    vertex(radius, -radius, 0, 1, 0);
-    vertex(radius, radius, 0, 1, 1);
-
-    endShape(CLOSE);
+    drawWedge(radius, i);
     pop();
   }
 }
 
+function drawWedge(r, index) {
+  let t = frameCount * 0.5 + index * 20;
+
+  let hue = (t * 2) % 360;
+  let sat = 80;
+  let bri = 100;
+
+  fill(hue, sat, bri, 80);
+
+  beginShape();
+  vertex(0, 0, 0);
+
+  for (let i = 0; i <= 10; i++) {
+    let a = map(i, 0, 10, -15, 15);
+    let rr = r * noise(i * 0.2, fr
