e3525973b3ace7f1a6f5d49e772367cb1e7f1cd5
scopeGL <seroleal@gmail.com>
2026-01-23 11:39:41 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 let kaleidoShader;
+let mode = 0; // 0 = vidrio pulido | 1 = facetado extremo
 
 function preload() {
   kaleidoShader = loadShader('kaleido.vert', 'kaleido.frag');
 }
 
 function setup() {
+  pixelDensity(window.devicePixelRatio || 1);
   createCanvas(windowWidth, windowHeight, WEBGL);
   noStroke();
 }
@@ -12,14 +14,18 @@ function setup() {
 function draw() {
   shader(kaleidoShader);
 
-  kaleidoShader.setUniform('u_resolution', [width, height]);
+  kaleidoShader.setUniform('u_resolution', [width * pixelDensity(), height * pixelDensity()]);
   kaleidoShader.setUniform('u_time', millis() / 1000.0);
+  kaleidoShader.setUniform('u_mode', mode);
 
   rect(-width / 2, -height / 2, width, height);
 }
-
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
+function keyPressed() {
+  if (key === '1') mode = 0; // vidrio pulido
+  if (key === '2') mode = 1; // facetado extremo
+}
